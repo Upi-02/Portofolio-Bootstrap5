85129cd807d4c7f0e96eac344e0951d98105f3e7
Upi-02 <luth88049@gmail.com>
2024-07-29 19:41:35 +0700

    #

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Parallax
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll +'%)'
+    })
+});
